96cd6eb0eff7896c7bd85a561e55b690e59da95d
oseipep <80490568+oseipep@users.noreply.github.com>
2022-01-19 10:35:35 -0500

    Delete R directory

--- R/addmed.R
+++
@@ -1,8 +0,0 @@
-#' @importFrom stats aggregate
-#' @keywords internal
-addmed <-
-function(data, vble, id) {
-	submed <- aggregate(data[,vble], list(id=data[,id]), meadian)
-	colnames(submed) <- c(id,"pmed")
-	merge(data, submed)
-}
